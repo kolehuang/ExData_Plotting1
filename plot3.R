d2ba548a72e714daea491ae187c83d1d0fae2a40
Kole Huang <yjhuang_18@hotmail.com>
2014-07-13 19:04:19 -0400

    Finalize package for Course Project 1

--- plot3.R
+++ plot3.R
@@ -4,7 +4,7 @@ Febdatetime <- paste(hpdata$Date, hpdata$Time)
 Ftime<- strptime(Febdatetime, format = "%d/%m/%Y %H:%M:%S")
 
 png(file = "plot3.png", bg = "transparent")
-plot(Ftime, hpdata$Sub_metering_1, type = "l", xlab = " ", ylab = "Engergy sub metering")
+plot(Ftime, hpdata$Sub_metering_1, type = "l", xlab = " ", ylab = "Energy sub metering")
 lines(Ftime, hpdata$Sub_metering_2, col = "red")
 lines(Ftime, hpdata$Sub_metering_3, col = "blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), lwd=c(1, 1, 1), col=c("black", "blue", "red")) 
--- plot4.R
+++ plot4.R
@@ -8,7 +8,7 @@ par(mfcol = c(2, 2))
 
 plot(Ftime, hpdata$Global_active_power, type="l", xlab = " ", ylab = "Global Active Power") #1st plot
 
-plot(Ftime, hpdata$Sub_metering_1, type = "l", xlab = " ", ylab = "Engerging sub metering")
+plot(Ftime, hpdata$Sub_metering_1, type = "l", xlab = " ", ylab = "Energy sub metering")
 lines(Ftime, hpdata$Sub_metering_2, col = "red")
 lines(Ftime, hpdata$Sub_metering_3, col = "blue")
 legend("topright", bty = "n", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), lwd=c(1, 1, 1), col=c("black", "blue", "red"))  #2nd plot
